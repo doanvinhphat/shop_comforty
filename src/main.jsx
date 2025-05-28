cf13c75504f6daeeb5b2b04bffc4456cdfc22cc7
unknown <PhatDoan6969@gmail.com>
2025-05-28 16:37:09 +0700

    Completed

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import MainLayout from './MainLayout/MainLayout'
+import "./index.css"
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <MainLayout />
+  </StrictMode>,
+)
